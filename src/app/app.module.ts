76f4438b203a21c5bad15666394f5509c42bf650
Kevin Y <paeynivek@gmail.com>
2023-07-06 01:47:31 -0700

    commit search component placeholder for route testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FolderFileListComponent } from './folder-file-list/folder-file-list.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FolderFileListComponent,
-    SidebarComponent
+    SidebarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
